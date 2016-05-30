193e1e9a18599d23a239fa4cec2b684e9d3d29b0
Mario César <marioclr.df@gmail.com>
2016-05-30 14:44:54 -0500

    Preferencias Versión 1.0

---
+++ app/src/main/java/com/mclr/mini/preferencias/MainActivity.java
@@ -0,0 +1,79 @@
+package com.mclr.mini.preferencias;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String NOMBRE="NOMBRE";
+    private EditText mEditTextNombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        String nombre = sharedPreferences.getString(NOMBRE,null);
+        if (nombre==null) {
+            textView.setText("Hola");
+        } else {
+            textView.setText("Bienvenido de vuelta " + nombre + "!");
+        }
+        mEditTextNombre = (EditText)findViewById(R.id.editTextNombre);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void guardaNombre(View view) {
+        SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+        editor.putString(NOMBRE, mEditTextNombre.getText().toString());
+        editor.commit();
+    }
+}
